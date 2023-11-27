231d657bafc5be7e60bb12b729d8bf0e44cf1812
Yeoony <83230013+jeongyummy@users.noreply.github.com>
2023-11-27 23:39:31 +0900

    [fix] 사이드메뉴 우선테스트

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -4,20 +4,27 @@ import Link from 'next/link'
 import styles from "@/app/scss/header/Header.module.scss"
 import { RxHamburgerMenu } from "react-icons/rx";
 import { useEffect, useState } from "react";
+import Sidebar from '../sidebar/Sidebar';
 
 
 export default function Header() {
     const [isLogin, setIsLogin] = useState(false);
+    const [menuToggle, setMenuToggle] = useState(false);
 
     // 소셜로그인
     const loginModalOpen = () => {
         console.log("login 구현중");
     }
+
+    //사이드 메뉴
+    const sidebarMenuToggle = () => {
+        console.log("넌뭐니 menuToggle:", menuToggle)
+    }
     
     return (
         <>
             <div className={styles.header_wrapper}>
-                <div>
+                <div onClick={() => {sidebarMenuToggle}}>
                     <span><RxHamburgerMenu size="24"/></span>
                 </div>
                 <div>
@@ -33,6 +40,8 @@ export default function Header() {
                     {isLogin ? <><span>마이페이지</span><span>로그아웃</span></> : <span onClick={() => (loginModalOpen)}>로그인</span> }
                 </div>
             </div>
+
+            { menuToggle && <Sidebar /> }
         </>
     )
 }
--- src/app/components/sidebar/Sidebar.tsx
+++ src/app/components/sidebar/Sidebar.tsx
@@ -5,14 +5,12 @@ import styles from "@/app/scss/sidebar/Sidebar.module.scss"
 
 export default function Sidebar() {
     return (
-        <>
-           <div className={styles.sidebar_wrapper}>
+           <nav className={styles.sidebar_wrapper}>
                 <ul>
                     <li><Link href={'/'} className={styles.sidebar_menu}>1</Link></li> 
                     <li><Link href={'/'} className={styles.sidebar_menu}>2</Link></li> 
                     <li><Link href={'/'} className={styles.sidebar_menu}>3</Link></li> 
                 </ul>
-           </div>
-        </>
+           </nav>
     )
 }
